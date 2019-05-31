58474f045f008a6038466599b8d095ac4f296631
CPrich905 <charlie.prichard@gmail.com>
2019-05-31 12:28:09 +0100

    Basic layout, player, enemy1

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
-const width = 5
+const width = 10
 const squares = []
-let playerIndex = Math.floor(width * width / 2) // will put player to center of grid
+let playerIndex = Math.floor(width * width - width) // will put player to bottom LHS of grid
+let alienIndex = 0 // starts alien in top LHS
 
 function movePlayer () {
   console.log(`the player should now move to position ${playerIndex}`)
@@ -22,28 +23,16 @@ function handleKeyDown(e) {
         playerIndex--
       }
       break
-    case 38:
-      if (playerIndex - width >= 0) {
-        playerIndex -= width
-      }
-      break
-    case 40:
-      if (playerIndex + width < width * width) {
-        playerIndex += width
-      }
-      break
     default:
       playerShouldMove = false
   }
   if (playerShouldMove) movePlayer()
 }
 
+
 function init() {
-  //  our code goes here
-  // find parent grid div
   const grid = document.querySelector('.grid')
   console.log('grid')
-
   //for loop to fill grid with squares
   for (let i = 0; i < width*width; i++) {
     const square = document.createElement('div')
@@ -55,8 +44,27 @@ function init() {
   }
 
   squares[playerIndex].classList.add('player')
-  window.addEventListener('keydown', handleKeyDown)
+  squares[alienIndex].classList.add('alien')
 
+  window.addEventListener('keydown', handleKeyDown)
+  document.getElementById('startBtn').addEventListener('click', alienMove)
+
+  // setInterval(alienMove, 1000)
+  // const stop = clearInterval(alienMove)
+  //
+  function alienMove() {
+    let alienShouldMove = true
+    console.log('alien should move')
+    setTimeout(alienShouldMove, 6000)
+    if (alienIndex % width < width-1) {
+      alienIndex++
+    } else if (alienIndex % width > 0) {
+      alienIndex--
+    } else {
+      alienShouldMove = false
+    }
+    if (alienShouldMove) alienMove()
+  }
 
 }
 
