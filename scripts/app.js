b9c71a84fbaaf9646d8be77fa1a8fb9b7f56d9ac
CPrich905 <charlie.prichard@gmail.com>
2019-06-03 11:00:37 +0100

    row numbers wrapping, alien & player moves, missile bugging

--- scripts/app.js
+++ scripts/app.js
@@ -1,68 +1,106 @@
 const width = 10
+const rows = []
 const squares = []
+const aliens = []
 let alienIndex = 0
-// let moving = false
 let movingRight = true
 let alienCount = 0 // counts movement
-let playerIndex = Math.floor(width * width - width) // will put player to bottom LHS of grid
+let playerIndex = Math.floor(width * width - 1) // will put player to bottom LHS of grid
 
 
 //ALIEN CONSTRUCTION
 
 class Alien {
-  constructor(rank, startingIndex, alienMove, alienHit, alienShoot) {
+  constructor(rank, startingIndex, position, moveCounter, alienHit, alienShoot) {
     this.rank = rank
     this.startingIndex = startingIndex
-    this.alienMove = alienMove
-    this.alienHit = alienHit
+    this.position = startingIndex+moveCounter
+    this.moveCounter = 0
+    this.alienhit = alienHit
     this.alienShoot = alienShoot
   }
+  moveEnemy() {
+    squares[this.position].classList.remove('alien')
+    this.position++
+    squares[this.position].classList.add('alien')
+  }
 }
+
 function handleKeyDown(e) {
   let playerShouldMove = true
   let missileShouldFire = false
   switch(e.keyCode) {
     case 39:
-    if (playerIndex % width < width-1) {
-      playerIndex++
-    }
-    break
+      if (playerIndex % width > 0 ) {
+        playerIndex--
+      }
+      break
     case 37:
-    if (playerIndex % width > 0) {
-      playerIndex--
-    }
-    break
+      if (playerIndex % width < width-1) {
+        playerIndex++
+      }
+      break
     case 32:
-    missileShouldFire = true
-    fireMissile()
-    break
+      missileShouldFire = true
+      fireMissile()
+      break
     default:
-    playerShouldMove = false
-    missileShouldFire = false
+      playerShouldMove = false
+      missileShouldFire = false
   }
   if (playerShouldMove) movePlayer()
   if (missileShouldFire) fireMissile()
 }
 
-// let startingIndex = ('alienIndex'+'rank')
-// let rank = null
-const alienOne = new Alien(0, 0, true, false, false)
-const alienTwo = new Alien(2, 1, true, false, false)
-const alienThree = new Alien(3, 2, true, false, false)
+
+//PLAYER MOVEMENT
+function movePlayer () {
+  squares.forEach(square => square.classList.remove('player'))
+  squares[playerIndex].classList.add('player')
+}
 
 
+// MISSILE MOVEMENT & TIMER
+function fireMissile () {
+  moveMissile()
+  const missileTimer = setInterval(moveMissile, 500)
+  setTimeout(()=> {
+    clearInterval(missileTimer)
+  }, 1500)
+}
 
+
+function moveMissile() {
+  let missilePosition = (playerIndex -= width)
+  console.log(missilePosition)
+  squares[this.missilePosition].classList.remove('missile')
+  this.missilePosition-=width
+  squares[this.missilePosition].classList.add('missile')
+  // squares[this.position].classList.remove('alien')
+  // this.position++
+  // squares[this.position].classList.add('alien')
+}
 
 function init() {
   // GRID INITIALISATION: for loop to fill grid with squares
   const grid = document.querySelector('.grid')
-  for (let i = 0; i < width*width; i++) {
-    const square = document.createElement('div')
-    square.classList.add('grid-item')
-    squares.push(square)
-    square.innerHTML = i
-    grid.append(square)
+  for (let i = 0; i < width; i++) {
+    const row = document.createElement('div')
+    row.classList.add('grid-row')
+    rows.push(row)
+    // row.innerHTML = i
+    grid.append(row)
   }
+  //GRID in ROW
+  rows.forEach((row, index) => {
+    for (let i = 0; i < width; i++) {
+      const square = document.createElement('div')
+      square.classList.add('grid-item')
+      squares.push(square)
+      square.innerHTML = index*width+i
+      row.append(square)
+    }
+  })
 
   // PAUSE BUTTON - doesn't fucking work!
   // function pauseGame() {
@@ -71,65 +109,35 @@ function init() {
   //   // let alienIndex = 0
   // }
 
-  // ALIEN BUILD
-  function formRanks() {
-    console.log('form Ranks')
-    squares[0].classList.add('alienOne')
-    squares[1].classList.add('alienTwo')
-    squares[2].classList.add('alienThree')
-  }
+
+  // ALIEN BUILD - pushing 3 to array
+  // function formRanks () {
+  //   console.log('form ranks')
+  aliens.push(new Alien(0, 0, true, false, false))
+  aliens.push(new Alien(2, 1, true, false, false))
+  aliens.push(new Alien(3, 2, true, false, false))
+
+  // }
+
 
   squares[playerIndex].classList.add('player')
-  squares[alienIndex].classList.add('alienOne')
-  const ranks = document.querySelector('#ranksBtn')
+  // const ranks = document.querySelector('#ranksBtn')
   const start = document.querySelector('#startBtn')
   // const pause = document.querySelector('#pauseBtn')
 
 
   // EVENT LISTENERS
   window.addEventListener('keydown', handleKeyDown)
-  ranks.addEventListener('click', formRanks)
+  // ranks.addEventListener('click', formRanks)
   start.addEventListener('click', play)
   // pause.addEventListener('click', pauseGame)
-
-  //PLAYER MOVEMENT
-
-
-  function movePlayer () {
-    squares.forEach(square => square.classList.remove('player'))
-    squares[playerIndex].classList.add('player')
-  }
-
-
-  // MISSILE MOVEMENT & TIMER
-  function fireMissile () {
-    moveMissile()
-    const missileTimer = setInterval(moveMissile, 500)
-    setTimeout(()=> {
-      clearInterval(missileTimer)
-    }, 1500)
-  }
-
-
-  function moveMissile() {
-    let missileIndex = (playerIndex -= width)
-    console.log(missileIndex)
-    squares[missileIndex].classList.add('missile')
-    // missileIndex += width
-    // squares.forEach(square => square.classList.remove('missile'))
-  }
 
 
 
   // ALIEN MOVEMENT & TIMER
-  function moveEnemy() {
-    squares.forEach(square => square.classList.remove('alienOne'))
-    squares[alienIndex].classList.add('alienOne')
-    // squares.forEach(square => square.classList.remove('alienTwo'))
-    // squares[1].classList.add('alienTwo')
-    // squares.forEach(square => square.classList.remove('alienThree'))
-    // squares[2].classList.add('alienThree')
-  }
+
+
+
   function play() {
     const enemyMovementTimer = setInterval(alienMove, 500)
     setTimeout(()=> {
@@ -142,25 +150,26 @@ function init() {
     if (movingRight) {
       console.log('aliens should move right')
       alienIndex ++
-      moveEnemy()
+      aliens[0].moveEnemy()
       alienCount ++
-    } else if (!movingRight) {
-      console.log('aliens should move left')
-      alienIndex --
-      moveEnemy()
-      alienCount --
     }
+    // else if (!movingRight) {
+    //   console.log('aliens should move left')
+    //   alienIndex --
+    //   moveEnemy()
+    //   alienCount --
+    // }
 
-    if (alienCount === 9) {
-      alienIndex += width+1
-      movingRight = false
-    } else if (alienCount === 0) {
-      alienIndex += width+1
-      movingRight = true
-    } else if (alienCount === 89) {
-      alienIndex = 89
-      alienCount = 0
-    }
+    // if (alienCount === 9) {
+    //   alienIndex += width+1
+    //   movingRight = false
+    // } else if (alienCount === 0) {
+    //   alienIndex += width+1
+    //   movingRight = true
+    // } else if (alienCount === 89) {
+    //   alienIndex = 89
+    //   alienCount = 0
+    // }
   }
 
 }
